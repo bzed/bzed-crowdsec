a386e1e2ffcf15ba423bbde9f27a95f7a59cdddd
Bernd Zeimetz <bernd@bzed.de>
2024-05-08 15:04:58 +0200

    fact: return certname/machine id list

--- lib/facter/crowdsec.rb
+++ lib/facter/crowdsec.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'yaml'
 
 Facter.add(:crowdsec) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
   setcode do
+    certnames_file = '/etc/crowdsec/crowdsec_machine_ids_to_certname.yaml'
     if Facter::Util::Resolution.which('cscli')
       output = {}
-      
+      if File.exists(certnames_file)
+        output['certnames'] = YAML.load_file(certnames_file)
+      end
+
       machines = Facter::Util::Resolution.exec('cscli machines list -o json')
       unless machines.to_s.strip.empty?
         output['machines'] = JSON.parse(machines)
