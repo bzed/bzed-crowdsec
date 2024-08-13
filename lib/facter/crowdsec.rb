664c3f1ebd8e1937555abca891183cf81a1f7d17
Bernd Zeimetz <bernd@bzed.de>
2024-08-13 14:58:10 +0200

    More info from cscli fact

--- lib/facter/crowdsec.rb
+++ lib/facter/crowdsec.rb
@@ -7,20 +7,18 @@ Facter.add(:crowdsec) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
   setcode do
     certnames_file = '/etc/crowdsec/crowdsec_machine_ids_to_certname.yaml'
+    requests = ['machines', 'bouncers', 'collections', 'scenarios', 'contexts', 'appsec-configs', 'appsec-rules']
     if Facter::Util::Resolution.which('cscli')
       output = {}
-      if File.exists?(certnames_file)
+      if File.exist?(certnames_file)
         output['certnames'] = YAML.load_file(certnames_file)
       end
 
-      machines = Facter::Util::Resolution.exec('cscli machines list -o json')
-      unless machines.to_s.strip.empty?
-        output['machines'] = JSON.parse(machines)
-      end
-
-      bouncers = Facter::Util::Resolution.exec('cscli bouncers list -o json')
-      unless machines.to_s.strip.empty?
-        output['bouncers'] = JSON.parse(bouncers)
+      requests.each do |request|
+        data = Facter::Util::Resolution.exec("cscli #{request} list -o json")
+        unless data.to_s.strip.empty?
+          output[request] = JSON.parse(data)
+        end
       end
       unless output.empty?
         output
