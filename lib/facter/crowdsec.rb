c35150de3c0a55521ae6a41d61fc7ffe646075ca
Bernd Zeimetz <bernd@bzed.de>
2024-08-30 16:57:25 +0200

    Use hub list in facter.
    This replaces multiple calls to cscli.

--- lib/facter/crowdsec.rb
+++ lib/facter/crowdsec.rb
@@ -7,18 +7,14 @@ Facter.add(:crowdsec) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
   setcode do
     certnames_file = '/etc/crowdsec/crowdsec_machine_ids_to_certname.yaml'
-    requests = ['machines', 'bouncers', 'collections', 'scenarios', 'contexts', 'appsec-configs', 'appsec-rules']
     if Facter::Util::Resolution.which('cscli')
       output = {}
       if File.exist?(certnames_file)
         output['certnames'] = YAML.load_file(certnames_file)
       end
-
-      requests.each do |request|
-        data = Facter::Util::Resolution.exec("cscli #{request} list -o json")
-        unless data.to_s.strip.empty?
-          output[request] = JSON.parse(data)
-        end
+      hub_data = Facter::Util::Resolution.exec('cscli hub list -o json')
+      unless data.to_s.strip.empty?
+        output.merge!(JSON.parse(hub_data))
       end
       unless output.empty?
         output
