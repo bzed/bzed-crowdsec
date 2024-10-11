eaf81576d8f25f6bac649edd6b80f31c5ae57cba
Bernd Zeimetz <bernd@bzed.de>
2024-10-11 14:05:56 +0200

    Lint happyness

--- manifests/local_api/register.pp
+++ manifests/local_api/register.pp
@@ -1,8 +1,15 @@
-# @summary Registers a machine at the local apu
+# @summary Registers a machine at the local api
 #
 # Registering machines at the local api is done using puppetdb_query,
 # so we only collect exported ressources. Don't try to use this define
 # directly.
+#
+# @param password
+# Password used to authenticate the machine
+#
+# @param machine_id
+# The id the machine is registered with. Do not use confidential strings if you
+# plan to connect to the (commercial/non local) crowdsec api.
 #
 # @example
 #   @@crowdsec::local_api::register { 'namevar':
@@ -12,7 +19,5 @@ define crowdsec::local_api::register (
   String $password,
   String $machine_id = $name,
 ) {
-
   fail('crowdsec::local_api::register should be used as exported resource only!')
-
 }
