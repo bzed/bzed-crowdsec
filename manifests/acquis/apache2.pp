37661c2e819a6c3b583894302b8442085b255e29
Bernd Zeimetz <bernd@bzed.de>
2025-01-23 17:16:37 +0100

    Add apache2 acquis class

---
+++ manifests/acquis/apache2.pp
@@ -0,0 +1,17 @@
+# @summary Handles acquisition of apache logfiles
+#
+# Takes care of default locations of apache2 logs, based on OS family
+#
+# @example
+#   include crowdsec::acquis::apache2
+class crowdsec::acquis::apache2 (
+  Array[Stdlib::Absolutepath] $filenames = $facts['os']['family'] ? {
+    'RedHat' => ['/var/log/httpd/*_log', '/var/log/httpd/*.log', '/var/log/httpd/*/*.log'],
+    default => ['/var/log/apache2/*.log', '/var/log/apache2/*/*.log'],
+  },
+) {
+  crowdsec::acquis::filenames { 'apache2':
+    filenames => $filenames,
+    type      => 'apache2',
+  }
+}
