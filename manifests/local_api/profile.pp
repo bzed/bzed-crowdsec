449dc2f4a8dc9093e7d122d028aa41f3bf8baf23
Bernd Zeimetz <bernd@bzed.de>
2025-02-06 13:41:18 +0100

    to_yaml addss --- lines, not needed twice

--- manifests/local_api/profile.pp
+++ manifests/local_api/profile.pp
@@ -21,7 +21,7 @@ define crowdsec::local_api::profile (
   include crowdsec::local_api
   concat::fragment { "crowdsec_local_api_profile_${name}":
     order   => $order,
-    content => "${config_yaml}---\n",
+    content => $config_yaml,
     target  => $crowdsec::local_api::profiles_file,
   }
 }
